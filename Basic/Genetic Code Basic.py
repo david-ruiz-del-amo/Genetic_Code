5f8bd1951998c9ca7851ec055178b1b9671b2336
David Ruiz del Amo <ruizdeladavid@gmail.com>
2026-05-04 11:29:52 +0200

    Update Genetic Code Basic.py

--- Basic/Genetic Code Basic.py
+++ Basic/Genetic Code Basic.py
@@ -1,9 +1,35 @@
 """
+Author: David Ruiz del Amo
+Last update: 04/05/2026
 This is a basic project which principal purpose is to translate a sequence in spite of not containing start codons or stop codons
 """
+import argparse #It will be used to get the arguments through the terminal
 #INPUTS
-#Receive the sequence
-seq = input(f"Enter a sequence:").upper().replace(" ", "") #Clean the sequence deleting all the spaces which the sequence may contain    
+#Receive the arguments by the user on the terminal
+parser = argparse.ArgumentParser(description="This is a basic project which principal purpose is to translate sequences in spite of not containing start codons or stop codons")
+parser.add_argument("--fasta", type=str, required=True,
+                    help="Introduce the path to the FASTA file which you want to process")
+fasta = parser.parse_args().fasta
+#Check if fasta has its extension
+if not fasta.endswith(".fasta"):
+    fasta += ".fasta"
+#Receive the sequence on a FASTA file
+with open(fasta) as f:
+    others = None
+    sequences = {} #This dictionary will store all the sequences on the FASTA
+    seq = []
+    for line in f:
+        if line.startswith(">"):
+            if others: #If there are more than one sequence, the sequences will be storing on a dictionary
+                sequences[name] = "".join(seq)
+                seq = []
+            else:
+                others = True 
+            name = line[1:].rstrip()
+        else:
+            seq.append(line.rstrip())
+    sequences[name] = "".join(seq) #Store the last sequence
+ 
 
 #FUNCTIONS
 #Create a function which returns the complementary strand of DNA
@@ -91,26 +117,28 @@ It may be redundant or inefficient, but to complete its purpose
 (tiny tasks like highschool/university homework) it is enough.
 """
 #Print the main information regardless which nucleic acid is
-print(50*"-") #Printing a line to make the output tidier and prettier
-print(f"Given sequence:    5' {spaced(seq)} 3'")
-print(f"Inverted sequence: 3' {spaced(seq[::-1])} 5'")
-print(f"Length: {len(seq)} bp")
-print(50*"-")
+for name, seq in sequences.items():
+    print(50*"-") #Printing a line to make the output tidier and prettier
+    print(name)
+    print(f"Given sequence:    5' {spaced(seq)} 3'")
+    print(f"Inverted sequence: 3' {spaced(seq[::-1])} 5'")
+    print(f"Length: {len(seq)} bp")
+    print(50*"-")
 
-#Print the dsDNA
-print("DNA:")
-print(f"5' {spaced(dna(seq))} 3'")
-print(f"3' {spaced(complementary_seq(dna(seq)))} 5'")
-print(50*"-")
+    #Print the dsDNA
+    print("DNA:")
+    print(f"5' {spaced(dna(seq))} 3'")
+    print(f"3' {spaced(complementary_seq(dna(seq)))} 5'")
+    print(50*"-")
 
-#Print the corresponding mRNA
-print("mRNA:")
-print(f"5' {spaced(mrna(seq))} 3'")
-print(50*"-")
+    #Print the corresponding mRNA
+    print("mRNA:")
+    print(f"5' {spaced(mrna(seq))} 3'")
+    print(50*"-")
 
-#Print the polypeptide information
-print("Polypeptide sequence:")
-polypeptide_seq = polypeptide(mrna(seq)) #Store the sequence in a variable so as not to call the function twice
-print(f"NH2 - ... - {polypeptide_seq} - ... - COOH")
-print(f"Polypeptide length: {len(polypeptide_seq.split(" - ")) - polypeptide_seq.count("Stop")} amino acids") #It must split again in order to count the amino acids
-print(50*"-")
+    #Print the polypeptide information
+    print("Polypeptide sequence:")
+    polypeptide_seq = polypeptide(mrna(seq)) #Store the sequence in a variable so as not to call the function twice
+    print(f"NH2 - ... - {polypeptide_seq} - ... - COOH")
+    print(f"Polypeptide length: {len(polypeptide_seq.split(" - ")) - polypeptide_seq.count("Stop")} amino acids") #It must split again in order to count the amino acids
+    print(50*"-")
