b1ecfaa70d16c2f7a5a370cd5f253a209ca68ae5
David Ruiz del Amo <ruizdeladavid@gmail.com>
2026-05-04 11:28:30 +0200

    Update Genetic Code Advanced.py

--- Advanced/Genetic Code Advanced.py
+++ Advanced/Genetic Code Advanced.py
@@ -1,33 +1,41 @@
 """
+Author: David Ruiz del Amo
+Last update: 04/05/2026
 This is an advanced project which purpose is to detect in a sequence a start codon and from that point, look for the biggest ORF and translate it
 However, this is not as advanced as it could be because do not take into account the mRNA splicing and other kinds of processings because it would be 
 tough to predict which splicing would occur
 """
-
+import argparse #It will be used to get the arguments through the terminal
 #INPUTS
-#Receive the nucleic acid and check if it is misspelled
-while True:
-    nucleic_acid = input("Please, enter a nucleic acid (DNA or RNA): ").upper().replace(" ", "") #Format the input
-    if nucleic_acid not in ("DNA", "RNA"):
-        print("Not valid input.")
-    else:
-        break
+#Receive the arguments by the user on the terminal
+parser = argparse.ArgumentParser(description="This is a basic project which principal purpose is to translate sequences")
+parser.add_argument("--nnaa", type=str, required=True, choices=("DNA", "RNA"),
+                    help="Introduce whichever nucleic acid you want to analyze (DNA or RNA)")
+parser.add_argument("--fasta", type=str, required=True,
+                    help="Introduce the path to the FASTA file which you want to process")
+nucleic_acid = parser.parse_args().nnaa
+fasta = parser.parse_args().fasta
+#Check if fasta has its extension
+if not fasta.endswith(".fasta"):
+    fasta += ".fasta"
 
-#Receive the sequence wich is going to be analyzed
-while True:
-    seq = input(f"Enter a {nucleic_acid} sequence:").upper().replace(" ", "") #Clean the sequence deleting all the spaces which the sequence may contain
-    
-    #Check if there are a confusion with the nucleic acid selected
-    if nucleic_acid == "DNA":
-        if "U" not in seq:
-            break
-        else: 
-            print("A DNA sequence must not contain U")
-    elif nucleic_acid == "RNA":
-        if "T" not in nucleic_acid:
-            break
-        else: 
-            print("A RNA sequence must not contain T")
+#Receive the sequence on a FASTA file
+with open(fasta) as f:
+    others = None
+    sequences = {} #This dictionary will store all the sequences on the FASTA
+    seq = []
+    for line in f:
+        if line.startswith(">"):
+            if others: #If there are more than one sequence, the sequences will be storing on a dictionary
+                sequences[name] = "".join(seq)
+                seq = []
+            else:
+                others = True 
+            name = line[1:].rstrip()
+        else:
+            seq.append(line.rstrip())
+    sequences[name] = "".join(seq) #Store the last sequence
+ 
 
 #FUNCTIONS
 #Create a function which returns the complementary strand of DNA
@@ -126,47 +134,49 @@ def spaced(seq):
     return " ".join(codon_list) #Returns a formatted string which has the same sequence, but it is separated to read better the sequence
 
 #OUTPUT
-#Print the main information regardless which nucleic acid is
-print(50*"-") #Printing a line to make the output tidier and prettier
-print(f"Given sequence:    5' {spaced(seq)} 3'")
-print(f"Inverted sequence: 3' {spaced(seq[::-1])} 5'")
-print(f"Length: {len(seq)} bp")
-print(50*"-")
-
-if nucleic_acid == "DNA":
-    #Print the dsDNA
-    print("DNA:")
-    print(f"5' {spaced(seq)} 3'")
-    print(f"3' {spaced(complementary_seq(seq))} 5'")
+for name, seq in sequences.items():
+    #Print the main information regardless which nucleic acid is
+    print(50*"-") #Printing a line to make the output tidier and prettier
+    print(name)
+    print(f"Given sequence:    5' {spaced(seq)} 3'")
+    print(f"Inverted sequence: 3' {spaced(seq[::-1])} 5'")
+    print(f"Length: {len(seq)} bp")
     print(50*"-")
 
-    #Print the corresponding mRNA
-    print("mRNA:")
-    print(f"5' {spaced(mrna(seq))} 3'")
-    print(50*"-")
+    if nucleic_acid == "DNA":
+        #Print the dsDNA
+        print("DNA:")
+        print(f"5' {spaced(seq)} 3'")
+        print(f"3' {spaced(complementary_seq(seq))} 5'")
+        print(50*"-")
 
-    #Print the polypeptide information
-    print("Polypeptide sequence:")
-    polypeptide_seq = polypeptide(mrna(seq)) #Store the sequence in a variable so as not to call the function twice
-    print(f"NH2 - ... - {polypeptide_seq} - ... - COOH")
-    print(f"Polypeptide length: {len(polypeptide_seq.split(" - ")) - polypeptide_seq.count("Stop")} amino acids") #It must split again in order to count the amino acids
-    print(50*"-")
-    
-elif nucleic_acid == "RNA":
-    #Print the dsDNA
-    print("DNA:")
-    print(f"5' {spaced(dna(seq))} 3'")
-    print(f"3' {spaced(complementary_seq(dna(seq)))} 5'")
-    print(50*"-")
+        #Print the corresponding mRNA
+        print("mRNA:")
+        print(f"5' {spaced(mrna(seq))} 3'")
+        print(50*"-")
 
-    #Print the corresponding mRNA
-    print("mRNA:")
-    print(f"5' {spaced(seq)} 3'")
-    print(50*"-")
+        #Print the polypeptide information
+        print("Polypeptide sequence:")
+        polypeptide_seq = polypeptide(mrna(seq)) #Store the sequence in a variable so as not to call the function twice
+        print(f"NH2 - ... - {polypeptide_seq} - ... - COOH")
+        print(f"Polypeptide length: {len(polypeptide_seq.split(" - ")) - polypeptide_seq.count("Stop")} amino acids") #It must split again in order to count the amino acids
+        print(50*"-")
+        
+    elif nucleic_acid == "RNA":
+        #Print the dsDNA
+        print("DNA:")
+        print(f"5' {spaced(dna(seq))} 3'")
+        print(f"3' {spaced(complementary_seq(dna(seq)))} 5'")
+        print(50*"-")
 
-    #Print the polypeptide information
-    print("Polypeptide sequence:")
-    polypeptide_seq = polypeptide(seq) #Store the sequence in a variable so as not to call the function twice
-    print(f"NH2 - ... - {polypeptide_seq} - ... - COOH")
-    print(f"Polypeptide length: {len(polypeptide_seq.split(" - ")) - polypeptide_seq.count("Stop")} amino acids") #It must split again in order to count the amino acids
-    print(50*"-")
+        #Print the corresponding mRNA
+        print("mRNA:")
+        print(f"5' {spaced(seq)} 3'")
+        print(50*"-")
+
+        #Print the polypeptide information
+        print("Polypeptide sequence:")
+        polypeptide_seq = polypeptide(seq) #Store the sequence in a variable so as not to call the function twice
+        print(f"NH2 - ... - {polypeptide_seq} - ... - COOH")
+        print(f"Polypeptide length: {len(polypeptide_seq.split(" - ")) - polypeptide_seq.count("Stop")} amino acids") #It must split again in order to count the amino acids
+        print(50*"-")
